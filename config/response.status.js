a3d9b791939f09c39b8778c8207177a6a84310f6
KYS-1026 <134212012+KYS-1026@users.noreply.github.com>
2024-01-19 04:32:14 +0900

    config 파일 업로드
    - db.config.js
    - error.js
    - response.js
    - response.status.js
    - swagger.config.js

---
+++ config/response.status.js
@@ -0,0 +1,30 @@
+// response.status.js
+
+import { StatusCodes } from "http-status-codes";
+
+export const status = {
+    // success
+    SUCCESS: {status: StatusCodes.OK, "isSuccess": true, "code": 2000, "message": "success!"}, 
+
+    // error
+	// common err
+    INTERNAL_SERVER_ERROR: {status: StatusCodes.INTERNAL_SERVER_ERROR, "isSuccess": false, "code": "COMMON000", "message": "서버 에러, 관리자에게 문의 바랍니다." },
+    BAD_REQUEST: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "COMMON001", "message": "잘못된 요청입니다." },
+    UNAUTHORIZED: {status: StatusCodes.UNAUTHORIZED, "isSuccess": false, "code": "COMMON002", "message": "권한이 잘못되었습니다." },
+    METHOD_NOT_ALLOWED: {status: StatusCodes.METHOD_NOT_ALLOWED, "isSuccess": false, "code": "COMMON003", "message": "지원하지 않는 Http Method 입니다." },
+    FORBIDDEN: {status: StatusCodes.FORBIDDEN, "isSuccess": false, "code": "COMMON004", "message": "금지된 요청입니다." },
+        
+    // member err
+    MEMBER_NOT_FOUND: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "MEMBER4001", "message": "사용자가 없습니다."},
+    NICKNAME_NOT_EXIST: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "MEMBER4002", "message": "닉네임은 필수입니다."},
+    ID_ALREADY_EXIST: {status: StatusCodes.CONFLICT, "isSuccess": false, "code": "MEMBER4003", "message": "ID가 이미 등록되어 있습니다."},
+    PARAMETER_IS_WRONG: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "MEMBER4004", "message": "파라미터 값이 잘못되었습니다.."},
+    STORE_NOT_EXIST: {status: StatusCodes.CONFLICT, "isSuccess": false, "code": "MEMBER4005", "message": "가게가 존재하지 않습니다."},
+    NOT_CHANGED_STATUS: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "MEMBER4006", "message": "완료로 바뀌지 않았습니다."},
+    
+    // 굳이 필요 없을 것 같기도 하지만 일단 추가 
+    SENTIMENT_ALREADY_EXIST: { status: StatusCodes.CONFLICT, "isSuccess": false, "code": "MEMBER4007", "message": "센티멘트 id가 이미 존재합니다."},
+    SENTIMENT_NOT_FOUND: { statis: StatusCodes.NOT_FOUND, "isSuccess": false, "code": "MEMBER4008", "message": "센티멘트가 존재하지 않습니다."},
+    // article err
+    ARTICLE_NOT_FOUND: {status: StatusCodes.NOT_FOUND, "isSuccess": false, "code": "ARTICLE4001", "message": "게시글이 없습니다."}
+}
