1e6573f080ada20547e0c84543ad8ae3c1bec158
wogh7572 <58222860+wogh7572@users.noreply.github.com>
2024-01-22 19:55:54 +0900

    Update search.response.dto.js

--- src/dtos/search.response.dto.js
+++ src/dtos/search.response.dto.js
@@ -1,36 +1,59 @@
-export const userSearchDTO = (data) => {
-    return { 
-        "nickname" : data.nickname, 
-        "created_at" : data.created_at,
-        "sentiment_title" : data.sentiment_title,
-        "book_title" : data.book_image,
-        "score" : data.score,
-        "content" : data.content,
-        "image_path": data.image_path,
-        "book_image": data.book_image
-    };
+export const bookSearchDTO = (data) => {
+    const booksArray = [];
+
+    for (let i = 0; i < 3; i++) { 
+        const bookObject = { 
+            "title": data.title,                            
+            "book_image": data.book_image,                    
+            "author": data.author,                                               
+            "publisher": data.publisher,                        
+            "pubdate": data.pubdate,
+            "avr_score": data.avr_score,
+            "eval_num": data.eval_num
+        };
+
+        booksArray.push(bookObject);
+    }
+
+    return booksArray;
 }
 export const sentimentSearchDTO = (data) => {
-    return { 
-        'sentiment_id' : body.sentiment_id,
-        'user_id' : body.user_id,
-        'book_title' : body.book_title,
-        'score' : body.score,
-        'content' : body.content,
-        'book_image' : body.book_image,
-        'season' : body.season
-    };
-}
-export const bookSearchDTO = (data) => {
-    return { 
-        "title":body.title,                            
-	    "book_image":body.book_image,                    
-		"author":body.author,                                               
-		"publisher":body.publisher,                        
-		"pubdate":body.pubdate,
-		"avr_score":body.avr_score,
-		"eval_num":body.eval_num
-    };
-}
+    const sentimentsArray = [];
 
+    for (let i = 0; i < 3; i++) { 
+        const sentimentObject = { 
+            'sentiment_id': data.sentiment_id,
+            'user_id': data.user_id,
+            'book_title': data.book_title,
+            'score': data.score,
+            'content': data.content,
+            'book_image': data.book_image,
+            'season': data.season
+        };
+
+        sentimentsArray.push(sentimentObject);
+    }
+
+    return sentimentsArray;
+}
+export const userSearchDTO = (data) => {
+    const usersArray = [];
+
+    for (let i = 0; i < 3; i++) { 
+        const userObject = { 
+            "nickname": data.nickname, 
+            "created_at": data.created_at,
+            "sentiment_title": data.sentiment_title,
+            "book_title": data.book_title,
+            "score": data.score,
+            "content": data.content,
+            "image_path": data.image_path,
+            "book_image": data.book_image
+        };
+
+        usersArray.push(userObject);
+    }
+
+    return usersArray;
+}
 
