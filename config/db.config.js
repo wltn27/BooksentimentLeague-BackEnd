20e10461008d5537210a281077336e9937097146
KYS-1026 <go1026s@naver.com>
2024-01-30 21:17:38 +0900

    정보 수정

--- config/db.config.js
+++ config/db.config.js
@@ -8,7 +8,7 @@ export const pool = mysql.createPool({
     user: process.env.DB_USER || 'root',  // user 이름
     port: process.env.DB_PORT || 3306,  // 포트 번호
     database: process.env.DB_TABLE || 'booksentimentleague',  // 데이터베이스 이름
-    password: process.env.DB_PASSWORD || 'bk202213035^^',  // 비밀번호
+    password: process.env.DB_PASSWORD || '',  // 비밀번호
     waitForConnections: true,
 		// Pool에 획득할 수 있는 connection이 없을 때,
 		// true면 요청을 queue에 넣고 connection을 사용할 수 있게 되면 요청을 실행하며, false이면 즉시 오류를 내보내고 다시 요청
