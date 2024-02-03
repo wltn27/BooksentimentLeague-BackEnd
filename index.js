52b7b6b3df6a17f5e738e115f236e43ad1045c5b
taeyeongkims <xodud8530@gmail.com>
2024-02-03 18:27:49 +0900

    test cors설정

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 // server setting - veiw, static, body-parser etc..
 app.set('port', process.env.PORT || 3000)   // 서버 포트 지정
 app.use((req, res) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "http://192.168.0.219:3000");
 });                            // cors 방식 허용
 app.use(express.static('public'));          // 정적 파일 접근
 app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
